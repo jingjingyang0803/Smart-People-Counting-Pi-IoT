ec66deb749a42e33ac0356efd3f2caecb91f3ecc
Jingjing Yang <104144565+jingjingyang0803@users.noreply.github.com>
2026-03-06 02:07:39 +0200

    Refactor MQTT client connection and publishing

--- communication/mqtt_client.py
+++ communication/mqtt_client.py
@@ -1,11 +1,14 @@
+# communication/mqtt_client.py
 import json
 import paho.mqtt.client as mqtt
 
 BROKER = "localhost"
+PORT = 1883
 TOPIC = "people_counting/data"
 
-client = mqtt.Client()
-client.connect(BROKER)
+_client = mqtt.Client()
+_client.connect(BROKER, PORT, 60)
+_client.loop_start()
 
 def publish(data):
-    client.publish(TOPIC, json.dumps(data))
+    _client.publish(TOPIC, json.dumps(data))
